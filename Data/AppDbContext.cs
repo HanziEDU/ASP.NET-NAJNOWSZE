1b3a188e51bd7d01952af88ee86192b3fd8beaf6
Hanzi <miki.handzlikpl@gmail.com>
2023-11-20 18:47:46 +0100

    Dodaj pliki projektów.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,96 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ContactEntity> Contacts { get; set; } 
+        public DbSet<OrganizationEntity> Organizations { get; set; }
+        private string Path { get; set; }  
+
+        public AppDbContext() 
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            Path = System.IO.Path.Join(path, "contacts.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"data source={Path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactEntity>()
+                .HasOne(e => e.Organization)
+                .WithMany(o => o.Contacts)
+                .HasForeignKey(e => e.OrganizationId);
+            modelBuilder.Entity<OrganizationEntity>()
+                .HasData(
+                new OrganizationEntity()
+                {
+                    Id = 101,
+                    Name = "WSEI",
+                    Description = "Uczelnia Wyższa"
+                },
+                new OrganizationEntity()
+                {
+                    Id = 102,
+                    Name = "Koło studenckie VR",
+                    Description = "Uczelnia Wyższa"
+                }
+                );
+
+            modelBuilder.Entity<ContactEntity>().HasData(
+                new ContactEntity() 
+                { 
+                    Id = 1, 
+                    Name = "Adam", 
+                    Email = "Adam@wsei.edu.pl", 
+                    Phone = "123456",
+                    OrganizationId = 102
+                },
+                new ContactEntity() 
+                { 
+                    Id = 2, 
+                    Name = "Ewa", 
+                    Email = "Ewa@wsei.edu.pl", 
+                    Phone = "654321",
+                    OrganizationId = 101
+                },
+                new ContactEntity() 
+                { 
+                    Id = 3, 
+                    Name = "Mikołaj",
+                    Email = "Mikołaj@wsei.edu.pl", 
+                    Phone = "999999" 
+                }
+                );
+            modelBuilder.Entity<OrganizationEntity>()
+                .OwnsOne(o => o.Address)
+                .HasData(
+                new
+                {
+                    OrganizationEntityId = 101,
+                    City = "Kraków",
+                    Street = "Św. Filipa 17",
+                    PostalCode = "31-150"
+                },
+                new
+                {
+                    OrganizationEntityId = 102,
+                    City = "Kraków",
+                    Street = "Św. Filipa 17, pok. 12",
+                    PostalCode = "31-150"
+                }
+                );
+        }
+    }
+}
---
+++ Data/Migrations/20231120174636_InitialCreate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_City = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_Street = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_PostalCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    contact_id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Birth = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    OrganizationId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.contact_id);
+                    table.ForeignKey(
+                        name: "FK_contacts_organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "organizations",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "contact_id", "Birth", "Email", "Name", "OrganizationId", "Phone" },
+                values: new object[] { 3, null, "Mikołaj@wsei.edu.pl", "Mikołaj", null, "999999" });
+
+            migrationBuilder.InsertData(
+                table: "organizations",
+                columns: new[] { "Id", "Description", "Name", "Address_City", "Address_PostalCode", "Address_Street" },
+                values: new object[,]
+                {
+                    { 101, "Uczelnia Wyższa", "WSEI", "Kraków", "31-150", "Św. Filipa 17" },
+                    { 102, "Uczelnia Wyższa", "Koło studenckie VR", "Kraków", "31-150", "Św. Filipa 17, pok. 12" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "contact_id", "Birth", "Email", "Name", "OrganizationId", "Phone" },
+                values: new object[,]
+                {
+                    { 1, null, "Adam@wsei.edu.pl", "Adam", 102, "123456" },
+                    { 2, null, "Ewa@wsei.edu.pl", "Ewa", 101, "654321" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contacts_OrganizationId",
+                table: "contacts",
+                column: "OrganizationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+
+            migrationBuilder.DropTable(
+                name: "organizations");
+        }
+    }
+}
